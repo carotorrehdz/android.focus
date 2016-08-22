068c63b9d0a3cf80216e5f14a4fbc76c74b2547e
carotorrehdz <cdelatorre@Carolinas-MacBook-Pro.local>
2016-08-21 23:00:55 -0500

    Correction

--- app/src/main/java/com/android/focus/ToolbarActivity.java
+++ app/src/main/java/com/android/focus/ToolbarActivity.java
@@ -1,6 +1,7 @@
 package com.android.focus;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -18,7 +19,13 @@ public class ToolbarActivity extends AppCompatActivity {
 
         Toolbar toolbar = ((Toolbar) findViewById(R.id.toolbar));
         setSupportActionBar(toolbar);
-        setTitle(title);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setTitle(title);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
     // endregion
 
